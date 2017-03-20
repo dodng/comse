eb1ad559a664896b65c64cdefb56401c96214845
dodng <dodng12@163.com>
2017-03-20 14:15:50 +0800

    add v0.6 build

--- core/build.sh
+++ core/build.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 CUR_PATH=`pwd`
-#build libevent
+#build libevent,just build one time
+tar xvzf libevent-1.4.14-stable.tar.gz
 cd $CUR_PATH"/libevent-1.4.14-stable/"
 ./configure
 make clean;make
 
-#build jsoncpp
-cd $CUR_PATH"/jsoncpp/"
-rm -rf $CUR_PATH"/jsoncpp/libs"
-scons platform=linux-gcc
-jsoncpp_lib=`find $CUR_PATH"/jsoncpp/libs/" -name "*.a"`
-
+#build comse
 cd $CUR_PATH
-rm -f libjsoncpp.a
-ln -s $jsoncpp_lib libjsoncpp.a
 make clean;make
